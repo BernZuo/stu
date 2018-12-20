229f7dc2659c998ab20906e2bb17bb6ae35f47d7
BernZuo <69089745@qq.com>
2018-12-20 15:18:28 +0800

    实时

--- stu/src/com/wenlong/stu/StuDemo.java
+++ stu/src/com/wenlong/stu/StuDemo.java
@@ -5,6 +5,5 @@ public class StuDemo {
 	public static void main(String[] args) {
 		System.out.println("============");
 		System.out.println("Hello World");
-		System.out.println("============");
 	}
 }
